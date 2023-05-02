26c4565dd249a6a3f008021b8f2a00eaf0a7dda8
FirozSkAhmad <you@example.com>
2023-05-02 12:29:55 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,44 @@
+{/* 
+<div id="parent">
+  
+   <div id="child">
+   <h1>I'm h1 tag</h1>
+   </div>
+
+</div> 
+*/}
+
+{/* 
+<div id="parent">
+  
+   <div id="child">
+   <h1>I'm h1 tag</h1>
+   <h2>I'm h1 tag</h2>
+   </div>
+
+</div> 
+*/}
+
+
+//creating neasted HTML using react
+// const parent = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" },
+//         React.createElement("h1", {}, "I'm h1 tag")));
+
+
+//To create sibling in HTML, we have to use [] in 3rd parameter
+
+const h1=React.createElement("h1", {}, "I'm h1 tag")
+const h2=React.createElement("h2", {}, "I'm h2 tag")
+
+const child = React.createElement("div", { id: "child" },
+    [h1, h2])
+
+const parent = React.createElement("div", { id: "parent" },
+    child);
+
+console.log(parent)//JS Object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
